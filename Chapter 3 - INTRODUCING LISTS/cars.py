a689d3d02d1b1532800d49a3ceb490b8c416e25b
Peter Njuguna <njugunapm23@gmail.com>
2025-11-28 15:05:09 +0300

    complete chapter 3 and challenge 3

---
+++ Chapter 3 - INTRODUCING LISTS/cars.py
@@ -0,0 +1,29 @@
+# Sorting a List Permanently with the sort() Method
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+# Sorting a List Temporarily with the sorted()
+# Function
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("Here is the original list again:")
+print(cars)
+
+# Printing a List in Reverse Order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+# Finding the Length of a List
+print(len(cars))
---
+++ Chapter 3 - INTRODUCING LISTS/challenge_3.py
@@ -0,0 +1,18 @@
+# 3-8. Seeing the World
+places_to_visit = ['Mt Kilimanjaro', 'China', 'Japan', 'USA', 'UK']
+print(places_to_visit)
+# list in alphabetical order
+print(sorted(places_to_visit))
+print(places_to_visit)
+# o print your list in reverse-alphabetical order without changing the order of the original list
+print(sorted(places_to_visit, reverse=True))
+print(places_to_visit)
+places_to_visit.reverse()
+print(places_to_visit)
+places_to_visit.sort()
+print(places_to_visit)
+places_to_visit.sort(reverse=True)
+print(places_to_visit)
+
+# 3-9. Dinner Guests
+print(len(places_to_visit))
