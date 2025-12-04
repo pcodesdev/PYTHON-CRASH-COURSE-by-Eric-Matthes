93548ca7c3366f64b27108b7222c36fcf287bc75
Peter Njuguna <njugunapm23@gmail.com>
2025-12-04 21:31:28 +0300

    start chapter 4 and complete challenge 1

---
+++ Chapter 4 - WORKING WITH LISTS/challenge_1.py
@@ -0,0 +1,10 @@
+# 4-1. Pizzas
+pizzas = ["Mango Pizza", "Beef Pizza", "Chicken Pizza"]
+for pizza in pizzas:
+    print(f"I like {pizza}.")
+print("I really love pizza!\n")
+# 4-2. Animals
+herbivorous_animals = ["cow", "sheep", "goat"]
+for animal in herbivorous_animals:
+    print(f"I like {animal}.")
+print("I really love herbivorous farm animals!\n")
---
+++ Chapter 4 - WORKING WITH LISTS/readme.md
@@ -0,0 +1,7 @@
+WORKING WITH LISTS
+When you want to do the same action
+with every item in a list, you can use Python’s for loop.
+Doing Something After a for Loop
+Avoiding Indentation Errors
+ logical error
+Indenting Unnecessarily After the Loop
